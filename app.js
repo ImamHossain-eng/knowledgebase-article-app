8eeceeaef00bc3b3ac744ff24cbba61cb0952d73
Babu Imam <babubpx2762@gmail.com>
2020-09-01 11:16:17 -0700

    Setup pug template engine

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 const express = require('express');
-
+const path = require('path');
+//init App
 const app = express();
+//Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
+
+//Home Route
 app.get('/', function(req, res) {
-    res.send('Hello World');
+    let articles = [
+        {
+            id: 1,
+            title: 'Article one',
+            author: 'Babu',
+            body: 'This is article one'
+        },
+        {
+            id: 2,
+            title: 'Article two',
+            author: 'Babu',
+            body: 'This is article two'
+        },
+        {
+            id: 3,
+            title: 'Article three',
+            author: 'Babu',
+            body: 'This is article three'
+        }
+    ];
+    res.render('index', {
+        title: 'Articles',
+        articles: articles
+    });
 });
-
+//Add Route
+app.get('/articles/add', function(req, res) {
+    res.render('add', {
+        title: 'Add Article'
+    });
+});
+//Start Server
 app.listen(3000, function() {
-    console.log('Server started');
+    console.log('Server started on 3000');
 });
