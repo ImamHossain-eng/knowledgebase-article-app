48a807e0dc3fc61ac2030e39f891b63b8b5c0412
Babu Imam <babubpx2762@gmail.com>
2020-09-02 01:42:18 -0700

    save article to db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const connectDB = require('./db/connection');
 
 //Connect Database
@@ -21,6 +22,10 @@ const Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+// body parser middleware parse application
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 //Home Route
 app.get('/', function(req, res) {
@@ -41,6 +46,24 @@ app.get('/articles/add', function(req, res) {
         title: 'Add Article'
     });
 });
+//Add submit post Route
+app.post('/articles/add', function(req, res) {
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+    //save to db
+    article.save(function(err){
+        if(err){
+            console.log(err);
+            return;
+        }else{
+            res.redirect('/');
+        }
+    });
+});
+
+
 //Start Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function() {
